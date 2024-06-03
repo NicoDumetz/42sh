4b478b508e39667469a473cd7e06c54068201fe2
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 14:19:10 +0200

    test: add unit test for globbings, var local, alias and history

--- Makefile
+++ Makefile
@@ -6,11 +6,7 @@
 ##
 
 SRC = main.c \
-	  src/lexer_parser/lexer/lexing.c\
-	  src/lexer_parser/lexer/lexing_features.c\
-	  src/lexer_parser/parser/parsing.c\
-	  src/feature_tab.c\
-	  src/globbings/globbings.c\
+	  src/lexer_parser/lexing.c\
 	  src/cd.c \
 	  src/env.c \
 	  src/setenv.c \
@@ -21,32 +17,42 @@ SRC = main.c \
 	  src/redirection/redirection_errors.c \
 	  src/command_errors.c\
 	  src/add_separator.c\
+	  src/globbings/globbings.c\
+	  src/globbings/assemble_simple.c\
+	  src/globbings/assemble_hard.c\
 	  tools/tab_len.c\
 	  tools/token_to_str_array.c\
-	  src/alias/alias.c \
-	  src/local/local_var.c
+	  tools/free_token.c
 
 OBJ = $(SRC:.c=.o)
 
-CFLAGS += -Llib -lmy -Iinclude -g
+CFLAGS += -Llib -lmy -Iinclude -g -Wall -Wextra
 
-NAME = mysh
+NAME = 42sh
+
 
 all: $(NAME)
 
 required:
-	make -C lib/my
+	@./tests/banner.sh
+	@printf "\e[1;32m"
+	@make -s -C lib/my
 
 $(NAME): required $(OBJ)
-	gcc -o $(NAME) $(OBJ) $(CFLAGS)
+	@gcc -o $(NAME) $(OBJ) $(CFLAGS)
 
 clean:
+	printf "\e[36m"
 	rm -f $(OBJ)
-	make clean -C lib/my
+	make -s clean -C lib/my
 
 fclean:
+	@printf "\e[35m"
 	rm -f $(NAME)
 	rm -f $(OBJ)
-	make fclean -C lib/my
+	make fclean -s -C lib/my
 
 re: fclean all
+
+run_test:
+	@./tests/tester.sh
--- include/minishell.h
+++ include/minishell.h
@@ -8,25 +8,37 @@
 #ifndef MINISHELL_HEADER
     #define MINISHELL_HEADER
     #include "my.h"
+    #include <stdio.h>
     #include <stdlib.h>
     #include <string.h>
     #include <stdbool.h>
     #include <glob.h>
-    #include <stdio.h>
     #define IS_ALPHA(c) (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
     #define IS_NUM(c) ('0' <= c && c <= '9')
 
-typedef struct alias_s {
-    char *name;
-    char *com;
-    struct alias_s *next;
-} alias_t;
+typedef enum lexing_s {
+    TERM_T = 0,
+    ARG_T,
+    SPACE_T,
+    SEMI_COL_T,
+    PIPE_T,
+    PARENTHESIS_LEFT_T,
+    PARENTHESIS_RIGHT_T,
+    REDIRECT_LEFT_T,
+    REDIRECT_RIGHT_T,
+    SINGLE_QUOTE,
+    DOUBLE_QUOTE_T,
+    BACKSLASH_T,
+    BACKSLASH_N_T,
+    BACkSLASH_T_T,
+    STAR_T,
+    SQUARE_BRACKET_LEFT_T,
+    SQUARE_BRACKET_RIGHT_T,
+    AND_T,
+    EXCLAM_POINT,
+    BACKTICK
+} lexing_t;
 
-typedef struct var_s {
-    char *var;
-    char *value;
-    struct var_s *next;
-} var_t;
 
 typedef struct token_s {
     char sep;
@@ -46,34 +58,18 @@ typedef struct garbage_s {
     char *raw_command;
     int return_value;
     token_t **token_list;
-    alias_t *alias;
-    var_t *local;
 } garbage_t;
 
-typedef struct redirection_tab_s {
+typedef struct feature_tab_s {
     char sep;
-    int (*redirection)(garbage_t *, token_t **);
-} redirection_tab_t;
-
-typedef struct lexing_tab_s {
-    char sep;
-    int (*lexing)(garbage_t *, token_t **);
-} lexing_tab_t;
-
-extern redirection_tab_t r_tab[];
-
-extern lexing_tab_t l_tab[];
-
+    void (*feature)(token_t **);
+} feature_tab_t;
+int assemble_hard(token_t **current, token_t **head);
+int tab_len(char **);
+void assemble_simple(token_t *current, token_t **head);
+int globbings(token_t **start, int index);
+void free_token(token_t *token);
 token_t **init_token_list(char *str);
-
-void lexing_features(garbage_t *garbage, token_t **token_list);
-
-int parsing_function(garbage_t *garbage, token_t **token_list);
-
-int globbings_function(garbage_t *garbage, token_t **token_list);
-
-int new_process(char **command, char **env);
-
 char **token_to_str_array(token_t *start, int end);
 void insert_spaces(char **input);
 void freeing(char *str, char **board);
@@ -84,19 +80,12 @@ char **copy_env(char **env);
 int show_env(char **env);
 int set_environnement(char *str, char ***env);
 int delete_env(char *str, char ***env);
+int new_process(char *str, char **env);
 int pipe_handling(char *str, char ***env, garbage_t *garbage);
-void pipe_redirect(int i, int num_pipe, int pipeline[][2]);
+void pipe_redirect(int i, int pipeline[][2]);
 void fork_pipes(char **pipes, int pipeline[][2], int num_pipe,
     garbage_t *garbage);
 int redirection(char *str, char ***env, int save_out);
 int redirection_errors(char *command, char **pipes, int i);
 int command_errors(char *str, char **pipes, int save_in, int save_out);
-int set_alias(char *name, char *command, garbage_t *garbage);
-void free_alias(garbage_t *garbage);
-char *check_alias(char *token, garbage_t *garbage);
-void unalias(char *name, garbage_t *garbage);
-int set_local(char *var, char *value, garbage_t *garbage);
-void unset_var(char *name, garbage_t *garbage);
-void free_var(garbage_t *garbage);
-char *check_local(char *token, garbage_t *garbage);
 #endif
